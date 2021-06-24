74e60172a629ca9ddf2316f73b80ff8b621d42d9
jBuly4 <ibuly4@gmail.com>
2021-06-24 21:28:19 +0300

    first attempt

--- chain_of_resp_solution.py
+++ chain_of_resp_solution.py
@@ -21,7 +21,7 @@ class NullHandler:
 
     def handle(self, obj, field_type):
         if self.__successor is not None:
-            return self.__successor.handle(obj, field_type)
+            return self.__successor.handle(obj, field_type) # it's not working without return
 
 
 class IntHandler(NullHandler):
@@ -32,33 +32,27 @@ class IntHandler(NullHandler):
             obj.integer_field = field_type.kind
             return obj.integer_field
         else:
-            print("FORWARD from INT")
-            return super().handle(obj, field_type)
+            return super().handle(obj, field_type) # it's working without return
 
 
 class FloatHandler(NullHandler):
     def handle(self, obj, field_type):
         if field_type.kind is float:
-            print("RUN FLOAT 1")
-            # print("FLOAT!", obj.float_field)
             return obj.float_field
         elif type(field_type.kind) == float:
-            print("RUN FLOAT 2")
             obj.float_field = field_type.kind
             # print("BUUUUU!", obj.float_field)
             return obj.float_field
         else:
-            print("FORWARD from FLOAT")
-            return super().handle(obj, field_type)
+            return super().handle(obj, field_type) # it's not working without return
 
 
 class StrHandler(NullHandler):
     def handle(self, obj, field_type):
         if field_type.kind is str:
-            print("STR!", obj.string_field)
             return obj.string_field
         elif type(field_type.kind) == str:
             obj.string_field = field_type.kind
             return obj.string_field
         else:
-            return super().handle(obj, field_type)
+            return super().handle(obj, field_type) # it's not working without return
--- tests.py
+++ tests.py
@@ -9,23 +9,17 @@ obj.string_field = "some text"
 chain = IntHandler(FloatHandler(StrHandler(NullHandler())))
 
 class TestChain(unittest.TestCase):
-    def test_chain_get_int(self):
-        case = 42
-        with self.subTest(x=case):
-            print(chain.handle(obj, EventGet(type(case))))
-            self.assertEqual(chain.handle(obj, EventGet(type(case))), case)
+    def test_chain_get(self):
+        cases = [42, 3.14, "some text"]
+        for case in cases:
+            with self.subTest(x=case):
+                self.assertEqual(chain.handle(obj, EventGet(type(case))), case)
 
-    def test_chain_get_float(self):
-        case = 3.14
-        with self.subTest(x=case):
-            # print(type(case) == float)
-            # print(chain.handle(obj, EventGet(type(case))))
-            self.assertEqual(chain.handle(obj, EventGet(type(case))), case)
-
-    def test_chain_get_str(self):
-        case = "some text"
-        with self.subTest(x=case):
-            self.assertEqual(chain.handle(obj, EventGet(type(case))), case)
+    def test_chain_set(self):
+        cases = [100, 0.5, "newest text"]
+        for case in cases:
+            with self.subTest(x=case):
+                self.assertEqual(chain.handle(obj, EventSet(case)), case)
 
 
 unittest.main()
